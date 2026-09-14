922a30c20c37d3ed75582d74ffe3c6587861cb07
CHENG LU <jason.cheng.lu@hotmail.com>
2026-09-15 06:24:49 +1200

    add TwoSum.c
    Signed-off-by: CHENG LU <jason.cheng.lu@hotmail.com>

---
+++ TwoSum/TwoSum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    int nums[] = {3, 7, 11, 15, 33, 12, 99, 1, 2, 4, 5};
+    int target = 9;
+    int returnSize;
+    int* result = twoSum(nums, 11, target, &returnSize);
+    
+    if (result != NULL) {
+        printf("Indices: [%d, %d]\n", result[0], result[1]);
+        free(result);
+    } else {
+        printf("No solution found.\n");
+    }
+    return 0;
+}
